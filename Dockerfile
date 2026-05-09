0f616a04dbba44039226e607d128ad9c88534f3f
SaNdr077 <sandronavrozashvili7@gmail.com>
2026-05-09 13:39:26 +0400

    backend version 2.4

--- Dockerfile
+++ Dockerfile
@@ -1,35 +1,18 @@
 FROM python:3.13-slim
 
-# სისტემური dependencies Chromium-ისთვის
 RUN apt-get update && apt-get install -y \
-    libglib2.0-0 \
-    libnss3 \
-    libnspr4 \
-    libatk1.0-0 \
-    libatk-bridge2.0-0 \
-    libcups2 \
-    libdbus-1-3 \
-    libexpat1 \
-    libdrm2 \
-    libxkbcommon0 \
-    libx11-6 \
-    libxcomposite1 \
-    libxdamage1 \
-    libxext6 \
-    libxfixes3 \
-    libxrandr2 \
-    libgbm1 \
     libpango-1.0-0 \
     libcairo2 \
-    libasound2 \
+    libgdk-pixbuf2.0-0 \
+    libffi-dev \
+    shared-mime-info \
     && rm -rf /var/lib/apt/lists/*
 
 WORKDIR /app
 
 COPY requirements.txt .
 RUN pip install -r requirements.txt
-RUN playwright install chromium
 
 COPY . .
 
-CMD python manage.py runserver 0.0.0.0:${PORT:-8000}
+CMD gunicorn cv_builder.wsgi --bind 0.0.0.0:${PORT:-8000} --workers 1
--- cv/services.py
+++ cv/services.py
@@ -18,45 +18,9 @@
         
 #         browser.close()
 #         return pdf_bytes
-from playwright.sync_api import sync_playwright
+
+from weasyprint import HTML
 
 def generate_resume_pdf(html_content):
-    with sync_playwright() as p:
-        browser = p.chromium.launch(
-            headless=True,
-            args=[
-                "--no-sandbox",
-                "--disable-setuid-sandbox",
-                "--disable-dev-shm-usage",
-                "--disable-gpu",
-            ]
-        )
-
-        page = browser.new_page()
-
-        # HTML load
-        page.set_content(html_content, wait_until="networkidle")
-
-        # full content height
-        content_height = page.evaluate("""
-            () => Math.max(
-                document.body.scrollHeight,
-                document.documentElement.scrollHeight
-            )
-        """)
-
-        # PDF generation (your original approach restored)
-        pdf_bytes = page.pdf(
-            width="210mm",
-            height=f"{content_height}px",
-            print_background=True,
-            margin={
-                "top": "0",
-                "right": "0",
-                "bottom": "0",
-                "left": "0"
-            }
-        )
-
-        browser.close()
-        return pdf_bytes
+    pdf_bytes = HTML(string=html_content).write_pdf()
+    return pdf_bytes
